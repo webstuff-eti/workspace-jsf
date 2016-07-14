3d6eaf2da00f092fbf9d91e0d475217ef46907bd
Tiago Tibães <tibaestiago@gmail.com>
2016-07-13 23:07:52 -0300

    Commit 02 - Configurando Hibernate

---
+++ register-competence/src/br/eti/webstuff/register/model/Competence.java
@@ -0,0 +1,42 @@
+package br.eti.webstuff.register.model;
+
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+public class Competence {
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String tecnologia;
+	
+	@Temporal(TemporalType.DATE)
+	private Calendar dataLancamento = Calendar.getInstance();
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTecnologia() {
+		return tecnologia;
+	}
+	public void setTecnologia(String tecnologia) {
+		this.tecnologia = tecnologia;
+	}
+	public Calendar getDataLancamento() {
+		return dataLancamento;
+	}
+	public void setDataLancamento(Calendar dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+}
